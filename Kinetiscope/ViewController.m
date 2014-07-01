904d7e30a436e4e564b12dfd36a35489db9c4d18
Cass Pangell <casspangell@Casss-MacBook-Pro.local>
2014-07-01 15:22:33 -0600

    Creates rows and columns of blocks.

--- Kinetiscope/ViewController.m
+++ Kinetiscope/ViewController.m
@@ -30,31 +30,22 @@
     [blockArray addObject:newBlock];
     NSLog(@"ar:%@", blockArray);
     
-    CGFloat blockAndBuffer = newBlock.getWidth + 5;
     CGFloat sideBuffer = 5;
+    CGFloat blockAndBuffer = newBlock.getWidth + sideBuffer;
     CGFloat topBuffer = 20;
     int blockRow = 0;
     int blockCol = 0;
+    int rowCount = 0;
     
     for(int i=0; i<[blockArray count]; i++){
         
-        newBlock.frame = CGRectMake(sideBuffer + (i * blockAndBuffer), topBuffer, newBlock.getWidth, newBlock.getHeight);
+        newBlock.frame = CGRectMake(sideBuffer + (rowCount * blockAndBuffer), topBuffer + (blockRow * (newBlock.getHeight + sideBuffer)), newBlock.getWidth, newBlock.getHeight);
 
-       /* if ((i % 3 == 0) && i != 0) {
-            topBuffer = topBuffer + newBlock.getHeight + sideBuffer;
-            newBlock.frame = CGRectMake(sideBuffer, topBuffer, newBlock.getWidth, newBlock.getHeight);
-            newBlock.backgroundColor = [UIColor blackColor];
-        }*/
-        
-        if (i == 3) {
-            blockRow = 1;
-            newBlock.frame = CGRectMake(sideBuffer, topBuffer + blockRow * (newBlock.getHeight + sideBuffer), newBlock.getWidth, newBlock.getHeight);
-        }
-        if (i == 4) {
-             newBlock.frame = CGRectMake(sideBuffer + newBlock.getWidth + sideBuffer, topBuffer + newBlock.getHeight + sideBuffer, newBlock.getWidth, newBlock.getHeight);
-        }
-        if (i == 5) {
-            newBlock.frame = CGRectMake(sideBuffer + newBlock.getWidth + sideBuffer + newBlock.getWidth + sideBuffer, topBuffer + newBlock.getHeight + sideBuffer, newBlock.getWidth, newBlock.getHeight);
+        if ((i % 3 == 0) && i != 0) {
+            rowCount = 0;
+            blockRow ++;
+        }else{
+            rowCount ++;
         }
         
         [self.view addSubview:newBlock];
